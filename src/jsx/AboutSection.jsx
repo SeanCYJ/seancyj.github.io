e9ed65e0b9105e1a960dd9e161633519fa657e3d
SeanCYJ <116822999+SeanCYJ@users.noreply.github.com>
2024-06-07 17:45:29 +0100

    add accordian component

--- src/jsx/AboutSection.jsx
+++ src/jsx/AboutSection.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { AccordianTemp } from './Accordian';
 import '/src/css/about.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const AboutSection = () => {    
+
+    const contentEdu = ["<b>MEng Aeronautics and Astronautics</b> </br> University of Southampton </br> Oct 2021 - Present", "<b>Cambridge A-Levels</b> <br/> Taylor's College <br/> Jan 2020 - Jun 2021"];
+    const contentSkills = ["SolidWorks, COMSOL, Ansys", "Python, Arduino", "html, CSS, JavaScript",  "Git"]
+    const contentInterests = ["Flight Simulator, Blender, Programming, Video Games"];
     return (
         <>
             <div className="row section-2">
@@ -17,8 +22,8 @@ const AboutSection = () => {
                                 I am a 3rd year MEng student with practical experience in engineering projects, alongside a passion in programming and
                                 design. I aim to apply my skills into diverse aerospace projects that can challenge my abilities, while enabling me to showcase my strengths and creativity
                                 in solving problems. 
-                            </span><br/><br/>
-                            <span className="abt-header">Education:</span>
+                            </span><br/><br/><br/>
+                            {/* <span className="abt-header">Education:</span>
                             <ul>
                                 <li>University of Southampton | MEng Aeronautics and Astronautics | Oct 2021 - Present</li>
                                 <li>Taylor's College | Cambridge A-Levels | Jan 2020 - Jun 2021</li>
@@ -31,7 +36,10 @@ const AboutSection = () => {
                             <span className="abt-header">Interests:</span>
                             <ul>
                                 <li>Flight Simulator, Blender, Programming, Video Games</li>
-                            </ul>
+                            </ul> */}
+                            <AccordianTemp index={0} title={'Education'} content={contentEdu}/>
+                            <AccordianTemp index={1} title={'Skills'} content={contentSkills}/>
+                            <AccordianTemp index={2} title={'Interests'} content={contentInterests}/>
                         </div>
                     </div>
                 </div>
---
+++ src/jsx/Accordian.jsx
@@ -0,0 +1,44 @@
+import React, {useState} from "react";
+import '/src/css/accordian.css';
+
+export function AccordianTemp({index, title, content}) {
+
+    const [Expand, setExpand] = useState(false);
+
+    const handleClick = () => {
+        setExpand(!Expand);
+    }
+
+    const UnpackContent = (contentPacked) => {
+        if (contentPacked.length === 1) {
+            return (
+                <ul>
+                    <li key={title+index} dangerouslySetInnerHTML={{ __html: content}}></li>
+                </ul>
+            );
+        }
+        return(
+            <ul>
+            {contentPacked.map((item, index) => (
+                <li key={title+index} dangerouslySetInnerHTML={{__html: item}}></li>
+            ))}
+            </ul>
+        );
+    
+    }
+
+    return(<>
+        <div className="accordian-cont" 
+           onClick={handleClick}
+        >
+           <button 
+           id={index}
+           className="accordian-title"
+           >+ {title}</button> 
+           <div 
+           id={index}
+           className={Expand ? "accordian-content expand" : "accordian-content"}
+           >{UnpackContent(content)}</div>
+        </div>
+    </>);
+}
