2d6b9127d462daeac1f5c9c72e4a1fa07a8356cb
SeanCYJ <116822999+SeanCYJ@users.noreply.github.com>
2024-06-08 13:27:30 +0100

    new file structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,10 @@
 import React, { lazy, Suspense} from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LandingSection from './jsx/LandingSection';
-import AboutSection from './jsx/AboutSection';
-import ExpSection from './jsx/ExperienceSection';
-import ContactSection from './jsx/ContactSection';
-// import { AwdJson } from './jsx/AwardsSection';
-// import { DarkModeSwitch } from './jsx/Switches';
-import NavBar from './jsx/Navbar';
-import { DarkModeProvider } from './jsx/DarkModeContext';
+import App from './App';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <>
-    <DarkModeProvider>
-      <Router>
-        <div>
-          <NavBar />
-          <Suspense fallback={<div>Loading...</div>}/>
-          <Routes>
-            <Route path="/" element={<LandingSection />} />
-            <Route path="/about" element={<AboutSection />} />
-            <Route path="/experience" element={<ExpSection />} />
-            <Route path="/contact" element={<ContactSection />} />
-          </Routes>
-        </div>
-      </Router>
-    </DarkModeProvider>
-  </>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
