ac0da32ac3dccb7a49e70b155e1719234ea6acb3
SeanChooYJ <116822999+SeanCYJ@users.noreply.github.com>
2024-10-07 09:43:06 +0100

    minor UI update

--- src/jsx/AboutSection.jsx
+++ src/jsx/AboutSection.jsx
@@ -1,12 +1,13 @@
-import React from "react";
+import React, {useState, useContext} from "react";
 import { AccordianTemp } from './Accordian';
 import '/src/css/about.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { DarkModeContext } from "./DarkModeContext";
 
 const AboutSection = () => {    
-
+    const {darkTheme} = useContext(DarkModeContext);
     const contentEdu = ["<b>MEng Aeronautics and Astronautics</b> </br> <b>(Specialization in Computational Engineering)</b> </br> University of Southampton </br> Oct 2021 - Present", "<b>Cambridge A-Levels</b> <br/> Taylor's College <br/> Jan 2020 - Jun 2021"];
-    const contentSkills = ["SolidWorks, COMSOL, Ansys, MATLAB", "Python, Arduino, Flutter, JS", "HTML, CSS, JS", "Blender",  "Git", "Microsoft Productivity Suite"]
+    const contentSkills = ["SolidWorks, COMSOL, Ansys, MATLAB", "C, Python, Arduino, Flutter, JS", "HTML, CSS, JS", "Blender",  "Git", "Microsoft Productivity Suite"]
     const contentInterests = ["Flight Simulator, Blender, Programming, Video Games"];
     return (
         <>
@@ -16,11 +17,11 @@ const AboutSection = () => {
                         About Me
                     </h2> */}
                     <div className="abt-cont">
-                        {/* <img className="abt-img" src={"../landingPicMain_2.svg"}/> */}
+                        <img className={ darkTheme ? "abt-img dm" : "abt-img"} src={"../drawing.png"}/>
+                        <span className="abt-name">Sean C. </span>
                         <div className="abt-text">
-                            <span className="abt-name">Sean C. </span><br/>
                             <span>
-                                I am a 3rd year MEng student with practical experience in engineering projects, alongside a passion in programming and
+                                I am a 4th year MEng student with practical experience in engineering projects, alongside a passion in programming and
                                 design. I aim to apply my skills into diverse aerospace projects that can challenge my abilities, while enabling me to showcase my strengths and creativity
                                 in solving problems. 
                             </span><br/><br/><br/>
